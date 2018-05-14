94b6b02b6e28e5b0512b029019ada65749d53e2c
Zack <lzcers@gmail.com>
2018-05-15 01:26:09 +0800

    refactor: 引入 eslint

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App.js'
-import { HashRouter as Router } from "react-router-dom"
+import { HashRouter as Router } from 'react-router-dom'
+import App from './App'
 
 ReactDOM.render((
     <Router>
--- src/views/Post/index.js
+++ src/views/Post/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
+
 const Post = props => (
     <p>Hello World</p>
 )
+
 export default Post
