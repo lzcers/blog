a2b85b52f5fe09509ad4cba75405193ced12bfdf
Zack <lzcers@gmail.com>
2022-02-12 18:26:02 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    outDir:  path.resolve(__dirname, './docs/'),
     rollupOptions: {
       output: {
         entryFileNames: `assets/[name].js`,
