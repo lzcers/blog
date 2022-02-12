3f0a57bfeb1747139f6dda213384af4524143c5f
Zack <lzcers@gmail.com>
2022-02-13 00:02:18 +0800

    update

--- src/pages/Aranya/index.tsx
+++ src/pages/Aranya/index.tsx
@@ -2,7 +2,7 @@ import './aranya.less'
 
 export default () => {
     return (
-        <div className="aranya heti heti--ancient">
+        <div className="aranya">
             <p>你来人间走一趟，不是要拥有什么，而是体验什么。</p>
         </div>
     );
