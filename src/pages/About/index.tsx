737c7c5fd8e1a3b2094a13f7ab0e2d36151e6ed8
Zack <lzcers@gmail.com>
2022-04-05 13:31:44 +0800

    update

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -8,25 +8,25 @@ const About = () => (
             <div className="myPic">
                 <img src={MyPic} alt="" />
                 <blockquote className="quotoSICP">
-                    <p>
-                        「如果说艺术解释了我们的梦想，那么计算机就是以程序的名义执行着它们。」
-                    </p>
+                    <p>「如果说艺术解释了我们的梦想，那么计算机就是以程序的名义执行着它们。」</p>
                     <p style={{ textAlign: 'right', fontStyle: 'italic', fontSize: '12px' }}>
                         —— Alan J.Perlis《计算机程序的构造与解释》
                     </p>
                 </blockquote>
             </div>
-            <strong><p className="name">陌上尘</p></strong>
-            <p>一只兴趣使然的程序猿</p>
-            <p>喜欢折腾各种有趣的东西</p>
-            <p>主业写前端，但从不把自己定为前端工程师</p>
-            <p>TypeScript、JavaScript、Rust、Go、Lisp...</p>
-            <p>
-                <a style={{ textDecoration: 'underline' }} href="https://github.com/lzcers">
-                    My GitHub
-                </a>
-            </p>
-            <i>lzcers@gmail.com</i>
+            <div className="introduction">
+                <p className="name"><strong>陌上尘</strong></p>
+                <p>一只兴趣使然的程序猿</p>
+                <p>喜欢折腾各种有趣的东西</p>
+                <p>主业写前端，但从不把自己定为前端工程师</p>
+                <p>TypeScript、JavaScript、Rust、Go、Lisp...</p>
+                <p><i>lzcers@gmail.com</i></p>
+                <p>
+                    <a style={{ textDecoration: 'underline' }} href="https://github.com/lzcers">
+                        My GitHub
+                    </a>
+                </p>
+            </div>
         </div>
         <div className="selfRight">
             <Guide />
