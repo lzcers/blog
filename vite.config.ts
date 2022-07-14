a0741435a978760bfb5b0c523f0c2a76986817bd
Zack <lzcers@gmail.com>
2022-07-14 09:13:44 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,9 @@ export default defineConfig({
             "@": path.resolve(__dirname, "./src"),
         },
     },
+    server: {
+        port: 3000,
+    },
     plugins: [
         react(),
         // viteStaticCopy({
