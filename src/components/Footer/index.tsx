a2abde4925b0c0ae37962c01fc3a5b9d18044968
Zack <lzcers@gmail.com>
2025-03-30 20:11:41 +0800

    update

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import "./footer.less";
 
 const Footer = () => (
     <footer>
         <p>
-            <span>Copyright © KSANA 2017 - 2024</span> &nbsp;&nbsp;
+            <span>Copyright © KSANA 2017 - 2025</span> &nbsp;&nbsp;
             <a href="https://beian.miit.gov.cn">浙ICP备2022004381号</a>
         </p>
     </footer>
